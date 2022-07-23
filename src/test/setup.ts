9e97cf6608d9065441082eae09b6d822dc37f13c
Nikhil Vijayan <nkhilv@gmail.com>
2022-07-23 17:59:59 +0100

    Introduce test context

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,51 +1,52 @@
 import test from 'ava'
-import type { TestFn } from 'ava'
+import type { TestFn, ExecutionContext, SerialFn } from 'ava';
 import server from '../server'
 import { GraphQLClient } from 'graphql-request'
 import { getSdk } from './generated'
 import type { Sdk } from './generated'
 
-type Setup = {
-  context: {
-    graphql: Sdk,
-  },
-  startServer: () => Promise<void>,
+type ServiceContext = {
+  graphql: Sdk,
 }
 
 async function sleep(milliseconds: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, milliseconds))
 }
 
-export function setupAva(): {
-  serial: TestFn['serial'],
-  test: TestFn
-} {
-  const { serial } = test
-
-  serial.before('Wait until graphql server initialises', async () => {
-    await sleep(1000)
-  })
-
-  return { serial, test }
-}
-
-export async function setup(): Promise<Setup> {
-  const portToListenOn = 4000
+function createContext(): ServiceContext {
+  const portToListenOn = 4000 // TODO: Replace this to use @ava/get-port
   const graphQlEndpoint = `http://localhost:${portToListenOn}/graphql`
 
   const client = new GraphQLClient(graphQlEndpoint)
   const sdk = getSdk(client)
 
   return {
-    context: {
-      graphql: sdk,
-    },
-    startServer: async () => {
-      server.listen({ port: portToListenOn }, error => {
-        if (error !== null) console.log('An error!')
-        console.log('server listening on 4000!')
-      })
-      await sleep(1000)
-    }
+    graphql: sdk,
+  }
+}
+
+export function setupAva() {
+  const { serial } = test as TestFn<ServiceContext>
+
+  test.before('Start the server', async () => {
+    await server.listen({ port: 4000 }, error => {
+      if (error !== null) console.log('An error!')
+      console.log('server listening on 4000!')
+    })
+    await sleep(1000)
+  })
+
+  test.before('Add context', async (t) => {
+    const context = createContext()
+    t.context = context
+  })
+
+  test.after.always('stop service', async () => {
+    await server.close()
+  })
+
+  return { 
+    serial,
+    test: (test as unknown) as TestFn<ServiceContext>,
   }
 }
